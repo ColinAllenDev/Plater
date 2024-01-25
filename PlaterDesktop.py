da2d598a9bc6e6203c29474605345fc91ede1044
Colin Allen <colinallen.dev@gmail.com>
2024-01-25 14:28:53 -0500

    v0.0

--- PlaterDesktop.py
+++ PlaterDesktop.py
@@ -1,4 +1,4 @@
-import configparser 
+from plater import Plater
 import tkinter as tk
 from tkinter import ttk
 import sv_ttk
@@ -10,58 +10,74 @@ class Application:
         self.root.title("Plater")
 
         # Create tabs container
-        self.notebook = ttk.Notebook(self.root)
+        notebook = ttk.Notebook(root)
 
-        self.tab_editor = ttk.Frame(self.notebook)
-        self.tab_options = ttk.Frame(self.notebook)
+        self.tab_editor = ttk.Frame(notebook)
+        self.tab_options = ttk.Frame(notebook)
 
-        self.notebook.add(self.tab_editor, text='Editor')
-        self.notebook.add(self.tab_options, text='Options')
+        notebook.add(self.tab_editor, text='Editor')
+        notebook.add(self.tab_options, text='Options')
 
-        ### EDITOR TAB ###
+        self.initialize_editor()
+        self.initialize_options()
+
+        # Pack notebook with tab data
+        notebook.pack(expand=True, fill='both')
+
+        # Submit Buttons
+        btn_save_svg = tk.Button(self.tab_editor, text='Export SVG', command=lambda: self.generate_image('svg'))
+        btn_save_svg.grid(row=7, column=2, columnspan=2, pady=10)
+
+    def initialize_editor(self):
         # Spacer
-        self.label_app = tk.Label(self.tab_editor, text="Plater", font=("Times New Roman", 24))
-        self.label_app.grid(row=0, column=1, pady=15)
+        label_app = tk.Label(self.tab_editor, text="Plater", font=("Times New Roman", 24))
+        label_app.grid(row=0, column=1, pady=15)
         
         # Title Field
-        self.label_title = tk.Label(self.tab_editor, text="Title")
+        label_title = tk.Label(self.tab_editor, text="Title")
+        label_title.grid(row=1, column=0, padx=10, pady=5, sticky=tk.E)
+
         self.input_title = tk.Entry(self.tab_editor)        
-        self.label_title.grid(row=1, column=0, padx=10, pady=5, sticky=tk.E)
         self.input_title.grid(row=1, column=1, padx=10, pady=5)
         
         # Tagline Field
-        self.label_tagline = tk.Label(self.tab_editor, text="Tagline")
+        label_tagline = tk.Label(self.tab_editor, text="Tagline")
+        label_tagline.grid(row=2, column=0, padx=10, pady=5, sticky=tk.E)
+
         self.input_tagline = tk.Entry(self.tab_editor)
-        self.label_tagline.grid(row=2, column=0, padx=10, pady=5, sticky=tk.E)
         self.input_tagline.grid(row=2, column=1, padx=10, pady=5)
         
         # Mount Field
-        self.label_mount = tk.Label(self.tab_editor, text="Mount")
+        label_mount = tk.Label(self.tab_editor, text="Mount")
+        label_mount.grid(row=3, column=0, padx=10, pady=5, sticky=tk.E)
+
         self.input_mount = tk.Entry(self.tab_editor)
-        self.label_mount.grid(row=3, column=0, padx=10, pady=5, sticky=tk.E)
         self.input_mount.grid(row=3, column=1, padx=10, pady=5)
         
-        # Title Field
-        self.label_length = tk.Label(self.tab_editor, text="Length")
+        # Length Field
+        label_length = tk.Label(self.tab_editor, text="Length")
+        label_length.grid(row=4, column=0, padx=10, pady=5, sticky=tk.E)
+
         self.input_length = tk.Entry(self.tab_editor)
-        self.label_length.grid(row=4, column=0, padx=10, pady=5, sticky=tk.E)
         self.input_length.grid(row=4, column=1, padx=10, pady=5)
         
         # Gain Field
-        self.label_gain = tk.Label(self.tab_editor, text="Gain")
+        label_gain = tk.Label(self.tab_editor, text="Gain")
+        label_gain.grid(row=5, column=0, padx=10, pady=5, sticky=tk.E)
+
         self.input_gain = tk.Entry(self.tab_editor)
-        self.label_gain.grid(row=5, column=0, padx=10, pady=5, sticky=tk.E)
         self.input_gain.grid(row=5, column=1, padx=10, pady=5)
         
         # Power Field
-        self.label_power = tk.Label(self.tab_editor, text="Power")
+        label_power = tk.Label(self.tab_editor, text="Power")
+        label_power.grid(row=6, column=0, padx=10, pady=5, sticky=tk.E)
+
         self.input_power = tk.Entry(self.tab_editor)
-        self.label_power.grid(row=6, column=0, padx=10, pady=5, sticky=tk.E)
         self.input_power.grid(row=6, column=1, padx=10, pady=5)
         
         # Metadata Field
-        self.label_metadata = tk.Label(self.tab_editor, text="Metadata")
-        self.label_metadata.grid(row=0, rowspan=2, column=2, padx=60, pady=5, sticky=tk.E)
+        label_metadata = tk.Label(self.tab_editor, text="Metadata")
+        label_metadata.grid(row=0, rowspan=2, column=2, padx=60, pady=5, sticky=tk.E)
         self.input_metadata_1 = tk.Entry(self.tab_editor)
         self.input_metadata_1.grid(row=1, column=2, padx=10, pady=5)
         self.input_metadata_2 = tk.Entry(self.tab_editor)
@@ -69,38 +85,50 @@ class Application:
         self.input_metadata_3 = tk.Entry(self.tab_editor)
         self.input_metadata_3.grid(row=3, column=2, padx=10, pady=5)
 
-        # Submit Button
-        self.input_submit = tk.Button(self.tab_editor, text='Save', command=self.generate_svg)
-        self.input_submit.grid(row=7, column=2, columnspan=2, pady=10)
-
-        ### OPTIONS TAB ###
+    def initialize_options(self):
         # Initialize options input fields
-        self.label_font_family = tk.Label(self.tab_options, text="Font")
+        label_font_family = tk.Label(self.tab_options, text="Font")
+        label_font_family.grid(row=1, column=0, padx=10, pady=5, sticky=tk.E)
+
         self.input_font_family = tk.Entry(self.tab_options)
-        self.label_font_family.grid(row=1, column=0, padx=10, pady=5, sticky=tk.E)
         self.input_font_family.grid(row=1, column=1, padx=10)
         self.input_font_family.insert(0, 'Bahnschrift')
 
-        self.label_path = tk.Label(self.tab_options, text="Path")
+        label_path = tk.Label(self.tab_options, text="Path")
+        label_path.grid(row=2, column=0, padx=10, pady=5, sticky=tk.E)
+        
         self.input_path = tk.Entry(self.tab_options)
-        self.label_path.grid(row=2, column=0, padx=10, pady=5, sticky=tk.E)
         self.input_path.grid(row=2, column=1, padx=10)
         self.input_path.insert(0, '')
 
-        self.notebook.pack(expand=True, fill='both')
+    def generate_image(self, type):
+        # Get Data
+        input_data = {}
+        input_data['font_family'] = self.input_font_family.get()
+        input_data['input_path'] = self.input_path.get()
+        input_data['title'] = self.input_title.get()
+        input_data['tagline'] = self.input_tagline.get()
+        input_data['mount'] = self.input_mount.get()
+        input_data['length'] = self.input_length.get()
+        input_data['gain'] = self.input_gain.get()
+        input_data['power'] = self.input_power.get()
+        input_data['metadata'] = [self.input_metadata_1.get(),
+                                  self.input_metadata_2.get(),
+                                  self.input_metadata_3.get()]
 
+        # Initialize plater API
+        plater = Plater()
+        plater.draw(input_data)
+        plate_path = plater.generate_svg(input_data)
 
         # Display success text
-        self.message_success = tk.Label(self.tab_editor,   text=f'Successfully saved {self.input_title.get()}')
-        self.message_success.grid(row=7, column=1, pady=5, sticky=tk.E)
+        message_success = tk.Label(self.tab_editor, text=f'Successfully saved {plate_path}')
+        message_success.grid(row=7, column=1, pady=5, sticky=tk.E)
 
 
 
-if __name__ == "PlaterDesktop":
-    # Read configuration file
-    config = configparser.ConfigParser().read('config.ini')['DEFAULT']
-    
-    root = tk.Tk()
-    app = Application(root)
-    sv_ttk.set_theme("dark")
-    root.mainloop()
+# Initialize desktop application
+root = tk.Tk()
+app = Application(root)
+sv_ttk.set_theme("dark")
+root.mainloop()
--- plater.py
+++ plater.py
@@ -1,58 +1,59 @@
-import configparser 
+import tomllib
 import drawsvg as draw
+import subprocess
 
 class Plater:
-    def __init__(self, config):
-        self.width = config['WidthIn'] * config['dpi']
-        self.height = config['HeightIn'] * config['dpi']
-        self.title_position = {'x': self.width / 2, 'y': config['FontSpacingMajor']}
-        self.tagline_position = {'x': self.width / 2, 'y': (self.title_position * 2) + config['FontSpacingMinor']}
-        self.length_position = {'x': config['FontSpacingMinor'], 'y': self.height - config['FontSpacingMajor']}
-        self.mount_position = {'x': config['FontSpacingMinor'], 'y': self.length_position['y'] - config['FontSpacingMinor']}
-        self.gain_position = {'x': self.width - config['FontSpacingMinor'], 'y': self.mount_position['y']}
-        self.power_position = {'x': self.width - config['FontSpacingMinor'] , 'y': self.length_position['y']}
+    def __init__(self):
+        with open('config.toml', 'rb') as f:
+            self.config = tomllib.load(f)
 
-    def generate_svg(self):
-        plate = draw.Drawing(self.width, self.height)
-        plate.set_render_size(self.width, self.height)
+        self.width = self.config['Width']
+        self.height = self.config['Height']
+        self.title_position = {'x': self.width / 2, 'y': self.config['FontSpacingMajor']}
+        self.tagline_position = {'x': self.width / 2, 'y': (self.title_position['y'] * 2) + self.config['FontSpacingMinor']}
+        self.length_position = {'x': self.config['FontSpacingMinor'], 'y': self.height - self.config['FontSpacingMajor']}
+        self.mount_position = {'x': self.config['FontSpacingMinor'], 'y': self.length_position['y'] - self.config['FontSpacingMajor']}
+        self.gain_position = {'x': self.width - self.config['FontSpacingMinor'], 'y': self.mount_position['y']}
+        self.power_position = {'x': self.width - self.config['FontSpacingMinor'] , 'y': self.length_position['y']}
 
-        # Placeholder frame
-        plate.append(draw.Rectangle(0, 0, self.width, self.height, fill_opacity=0, stroke='white'))
+    def draw(self, data):
+        self.plate = draw.Drawing(self.width, self.height, origin=(0, 0),
+                                  context=None, animation_config=None, id_prefix='d')
+        self.plate.set_render_size(self.width, self.height)
 
         # Title
-        plate.append(draw.Text(self.input_title.get(), 
-                               font_size=config['FontSizeTitle'], 
-                               font_family=self.input_font_family.get(), 
+        self.plate.append(draw.Text(data['title'], 
+                               font_size=self.config['FontSizeTitle'], 
+                               font_family=data['font_family'], 
                                center=True, 
                                fill='white', 
                                x=self.title_position['x'], 
                                y=self.title_position['y']))
 
+        
         # Tagline
-        plate.append(draw.Text(self.input_tagline.get(),
-                               font_size=config['FontSizeTagline'],
-                               font_family=self.input_font_family.get(),
+        self.plate.append(draw.Text(data['tagline'],
+                               font_size=self.config['FontSizeTagline'],
+                               font_family=data['font_family'],
                                fill='white',
                                center=True, 
                                x=self.tagline_position['x'],
                                y=self.tagline_position['y']))
 
         # Metadata
-        plate.append(draw.Text([self.input_metadata_1.get(),
-                                self.input_metadata_2.get(),
-                                self.input_metadata_3.get()],
-                               font_size=config['FontSizeMetadata'],
-                               font_family=self.input_font_family.get(),
+        self.plate.append(draw.Text(data['metadata'],
+                               font_size=self.config['FontSizeMetadata'],
+                               font_family=data['font_family'],
                                fill='white',
                                center=True,
                                text_anchor='middle',
                                x=self.width / 2,
-                               y=self.width / 2))
+                               y=self.height / 2))
 
         # Mount
-        plate.append(draw.Text(f'Mount: {self.input_mount.get()}',
-                               font_size=config['FontSizeInfo'],
-                               font_family=self.input_font_family.get(),
+        self.plate.append(draw.Text(f'Mount: {data["mount"]}',
+                               font_size=self.config['FontSizeInfo'],
+                               font_family=data['font_family'],
                                fill='white',
                                center=True,
                                text_anchor='start',
@@ -60,9 +61,9 @@ class Plater:
                                y=self.mount_position['y']))
 
         # Length
-        plate.append(draw.Text(f'Length: {self.input_length.get()}',
-                               font_size=config['FontSizeInfo'],
-                               font_family=self.input_font_family.get(),
+        self.plate.append(draw.Text(f'Length: {data["length"]}',
+                               font_size=self.config['FontSizeInfo'],
+                               font_family=data['font_family'],
                                fill='white',
                                center=True,
                                text_anchor='start',
@@ -70,9 +71,9 @@ class Plater:
                                y=self.length_position['y']))
 
         # Gain
-        plate.append(draw.Text(f'Gain: {self.input_gain.get()}',
-                               font_size=config['FontSizeInfo'],
-                               font_family=self.input_font_family.get(),
+        self.plate.append(draw.Text(f'Gain: {data["gain"]}',
+                               font_size=self.config['FontSizeInfo'],
+                               font_family=data['font_family'],
                                fill='white',
                                center=True,
                                text_anchor='end',
@@ -80,27 +81,28 @@ class Plater:
                                y=self.gain_position['y']))
 
         # Power
-        plate.append(draw.Text(f'Power: {self.input_power.get()}',
-                               font_size=config['FontSizeInfo'],
-                               font_family=self.input_font_family.get(),
+        self.plate.append(draw.Text(f'Power: {data["power"]}',
+                               font_size=self.config['FontSizeInfo'],
+                               font_family=data['font_family'],
                                fill='white',
                                center=True,
                                text_anchor='end',
                                x=self.power_position['x'],
                                y=self.power_position['y']))
-        # Save svg
-        final_path = ''
-        if (self.input_path.get() == ''):
-            final_path = f'{self.input_title.get()}.svg';
-        else:
-            final_path = f'{self.input_path.get().rstrip("/")}/{self.input_title.get().replace("/", "-")}.svg'
-        plate.save_svg(final_path)
+        
+    def generate_svg(self, data):
+        try:
+            final_path = ''
+            if (data["input_path"] == ''):
+                final_path = f'{data["title"]}.svg';
+            else:
+                final_path = f'{data["input_path"].rstrip("/")}/{data["title"].replace("/", "-")}.svg'
+            
+            self.plate.save_svg(final_path)
+            
+            subprocess.call(f'inkscape --file={final_path} --export-plain-svg={final_path} --export-text-to-path', shell = True)
 
-        # Display success text
-        self.message_success = tk.Label(self.tab_editor,   text=f'Successfully saved {self.input_title.get()}')
-        self.message_success.grid(row=7, column=1, pady=5, sticky=tk.E)
-
-
-# Main
-config = configparser.ConfigParser().read('config.ini')['DEFAULT']
-
+            
+            return final_path
+        except Exception as exc:
+            raise RuntimeError("Something went wrong!") from exc
